6a7620653b2e39675bcb006fb51bef27529ef160
mangabot <yamaoka.jirei@gmail.com>
2018-08-16 17:07:22 +0900

    update

--- output_frame_labels.py
+++ output_frame_labels.py
@@ -2,9 +2,11 @@ import numpy as np
 import pandas as pd
 import os
 
+
 ori_anno = pd.read_csv('../../Ano..', sep=" ", delimiter="\t", usecols=[2,3,5], header=None, names=["start", "end", "label"])
-action_labels = pd.read_csv('./labels.txt')
+action_labels = pd.read_csv('./labels.txt', header=None, sep='\t')
 index = {l:i for i,l in enumerate(labels[0])}
+
 
 images_list = os.listdir('../Annotation/youtube')
 
